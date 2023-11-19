35ca841fd766b701c7f8c5aedac50fb0fcff55f3
camilo <camiloa.fetecuam@ecci.edu.co>
2023-11-19 10:30:54 -0500

    add new portfolio

--- script.js
+++
@@ -1,8 +0,0 @@
-// JavaScript para activar/desactivar el menú desplegable
-const menuToggle = document.querySelector('.menu-toggle');
-const sidebar = document.querySelector('.sidebar');
-
-menuToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('active'); // Agrega o quita la clase "active" al menú
-    menuToggle.classList.toggle('active'); // Agrega o quita la clase "active" al botón de menú
-});
